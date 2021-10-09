86d7637b65e27cf802de3325f665da7af760c6ab
Tomas Roy <hello@minicreative.net>
2021-10-08 21:45:19 -0500

    vessel-scanner adds entries to database

--- vessel-scanner/README.md
+++ vessel-scanner/README.md
@@ -4,4 +4,8 @@ Uses a serial port connection to detect incoming messages from an AIS Receiver
 
 ### Enviornment Variables
 
-- **SERIAL_PORT**: path to AIS receiver (do `ls /dev/*usb*`, use listing with `tty`)
+- **AIS_SERIAL_PORT**: path to AIS receiver (do `ls /dev/*usb*`, use listing with `tty`)
+- **MONGO_DB**: host of Mongo database
+- **MONGO_DB**: name of Mongo database
+- **MONGO_USER**: username for Mongo database
+- **MONGO_PASS**: username for Mongo password
--- vessel-scanner/index.js
+++ vessel-scanner/index.js
@@ -1,16 +1,58 @@
 const SerialPort = require('serialport')
 const AisDecoder = require('ggencoder').AisDecode
+const { MongoClient } = require("mongodb")
+
+// Validate environment variables
+const req_env = ["MONGO_HOST", "MONGO_DB", "MONGO_USER", "MONGO_PASS", "AIS_SERIAL_PORT"]
+for (let env of req_env) if (process.env[env] === undefined) throw(`Enviornment variable ${env} is required`)
+
+// Setup Mongo client
+let db;
+console.log("Connecting to database...")
+let mongoClient = new MongoClient(
+    `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`
+)
+mongoClient.connect((err) => {
+    if (err) throw(err)
+    db = mongoClient.db(process.env.MONGO_DB)
+    console.log("Connected to database!")
+})
 
 // Setup serial reader
-let serialPort = new SerialPort(process.env.SERIAL_PORT, {
+console.log(`Streaming AIS data from ${process.env.AIS_SERIAL_PORT}...`)
+let serialPort = new SerialPort(process.env.AIS_SERIAL_PORT, {
     baudRate: 38400
 })
 
 // Listen to serial reader
 serialPort.on('data', (buffer) => {
+    console.log(`\nAIS packet received ${buffer.toString().replace("\n", "")}`)
+
+    // Attempt to decode packet
     var decoded = new AisDecoder(buffer.toString())
-    if (decoded.valid) console.log ('%j', decoded)
+    if (!decoded.valid) return console.error("Invalid packet")
+
+    // Attempt to write relevant entries to database
+    // TODO: Refine filter (only log entries from boats we care about?)
+    // TODO: Don't save duplicate entries (when mmsi entry with lat, long, status, speed not changed, requires a db query)
+    if (db) {
+        db.collection("ais").insertOne({
+            timestamp: Date.now(),
+            mmsi: decoded.mmsi,
+            location: {
+                type: "Point",
+                coordinates: [decoded.lon, decoded.lat]
+            },
+            status: decoded.navstatus,
+            speed: decoded.sog
+        }).then(result => {
+            console.log(`Saved entry for ${decoded.mmsi} in database`)
+        }).catch(err => {
+            console.error(err)
+        })
+    }
 })
 serialPort.on('error', (err) => {
     console.error(err)
 })
+
