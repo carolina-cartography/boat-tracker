f8761d2f28a6872d0d4484717627bac685462afb
Tomas Roy <hello@minicreative.net>
2021-11-16 19:39:03 -0600

    travis script updated

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 if [[ "$1" == *"ferry-scraper"* ]]; then
     echo "Building ferry-scraper..."
+    docker build -t "minicreative/ferry-scraper:$TRAVIS_BUILD_NUMBER" ./ferry-scraper
+    docker tag minicreative/ferry-scraper:$TRAVIS_BUILD_NUMBER minicreative/ferry-scraper:latest
 elif [[ "$1" == *"vessel-scanner"* ]]; then
     echo "Building vessel-scanner..."
+    docker buildx build --platform linux/arm/v7 -t "minicreative/vessel-scanner:latest" ./vessel-scanner
 elif [[ "$1" == *"app"* ]]; then
     echo "Building app..."
+    docker build -t "minicreative/boat-tracker:$TRAVIS_BUILD_NUMBER" ./app
+    docker tag minicreative/boat-tracker:$TRAVIS_BUILD_NUMBER minicreative/boat-tracker:latest
 else
     echo "No matching build"
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 if [[ "$1" == *"ferry-scraper"* ]]; then
     echo "Deploying ferry-scraper..."
+    docker image push --all-tags minicreative/ferry-scraper
+    kubectl set image cronjob/ferry-scraper-cron "main=minicreative/ferry-scraper:$TRAVIS_BUILD_NUMBER"
 elif [[ "$1" == *"vessel-scanner"* ]]; then
     echo "Deploying vessel-scanner..."
+    docker image push minicreative/vessel-scanner:latest
 elif [[ "$1" == *"app"* ]]; then
     echo "Deploying app..."
+    docker image push --all-tags minicreative/boat-tracker
+    kubectl set image deployment/boat-tracker "main=minicreative/boat-tracker:$TRAVIS_BUILD_NUMBER"
 else
     echo "No matching build"
 fi
