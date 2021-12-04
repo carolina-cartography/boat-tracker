2ccd32247ae8ddb5ed1e311ae3c2b41cb1e5541b
Tomas Roy <hello@minicreative.net>
2021-12-03 20:56:20 -0600

    app: started ferry trip processing algorithm, timezone fixes (#2)
    * app: ferry list displays departure time
    * small changes

--- app/public/main.js
+++ app/public/main.js
@@ -1,5 +1,6 @@
 const CENTER = [18.16831061820438, -65.54537588810639]
 const ZOOM = 12
+const REFRESH_INTERVAL = 15000
 
 const BOAT_SVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><path class="accent" d="M336.4,231.3c-0.1-44.1-7.7-78.3-13.9-99.7c-10.1-35-22.9-57.7-35.1-73.8c-19.2-25.5-30.6-26.2-33.4-26.2c-5.2,0-15.8,1.7-34.5,26.2c-9.6,12.5-22.6,33.6-34,73.8c-6.3,22.2-13.6,55.9-14.8,98.7"/><rect x="170.9" y="225" class="accent" width="165.6" height="193.2"/><path class="primary" d="M327.2,250.6c-0.1-45-6.8-79.8-12.3-101.7c-9-35.7-20.3-58.9-31.1-75.3c-17.1-26-27.2-26.7-29.7-26.8c-4.6,0-14,1.8-30.7,26.8c-8.5,12.8-20.1,34.3-30.2,75.3c-5.6,22.6-12,57-13.1,100.8L327.2,250.6z"/><rect x="193.6" y="260.4" class="accent" width="120.1" height="38.7"/><path class="accent" d="M316.6,481.4H190.7c-11,0-19.8-8.9-19.8-19.8v-44.1c0-11,8.9-19.8,19.8-19.8h125.9c11,0,19.8,8.9,19.8,19.8v44.1C336.4,472.5,327.6,481.4,316.6,481.4z"/><path class="primary" d="M310.1,472.7H197.2c-9.4,0-17.1-7.7-17.1-17.1V248.4c0-9.4,7.7-17.1,17.1-17.1h112.9c9.4,0,17.1,7.7,17.1,17.1v207.2C327.2,465,319.5,472.7,310.1,472.7z"/><path class="accent" d="M295.7,453h-84.2c-7.9,0-14.4-6.4-14.4-14.4V245.6c0-7.9,6.4-14.4,14.4-14.4h84.2c7.9,0,14.4,6.4,14.4,14.4v193.1C310.1,446.6,303.7,453,295.7,453z"/></svg>`
 
@@ -7,10 +8,12 @@ let map, markers
 
 $(document).ready(() => {
     dayjs.extend(window.dayjs_plugin_relativeTime)
+    dayjs.extend(window.dayjs_plugin_utc)
+    dayjs.extend(window.dayjs_plugin_timezone)
 
 	initializeMap()
     load()
-    setInterval(load, 15000)
+    setInterval(load, REFRESH_INTERVAL)
 })
 
 function initializeMap() {
@@ -42,17 +45,21 @@ function load() {
     loadTrips()
 }
 
-function getHTMLForTrips(items, past) {
-    if (items.length < 1 && !past) return "No more ferries today"
-    let html = "";
-    for (let item of items) {
+function getHTMLForTrips(trips, past) {
 
-        let port = item.direction === "outbound" ? "Vieques" : "Ceiba"
+    // Validate HTML from server
+    if (trips.length < 1 && !past) return "No more ferries today"
+
+    let html = "";
+    for (let trip of trips) {
+
+        let port = trip.direction === "outbound" ? "Vieques" : "Ceiba"
 
         html += "<div class='trip'>"
-            html += "<div><span class='title'>Time</span>" + dayjs(item.date).format("MM/DD hh:mma") + "</div>"
+            html += "<div><span class='title'>Time</span>" + dayjs(trip.date).format("MM/DD hh:mma") + "</div>"
             html += "<div><span class='title'>Port</span>" + port + "</div>"
-            html += "<div><span class='title'>Vessel</span>" + item.vessel + "</div>"
+            html += "<div><span class='title'>Vessel</span>" + trip.vessel + "</div>"
+            if (trip.timeInMotion) html += "<div><span class='title'>Departure</span>" + dayjs(trip.timeInMotion).tz("America/Puerto_Rico").format("hh:mma") + "</div>"
         html += "</div>"
     }
     return html
@@ -61,12 +68,12 @@ function getHTMLForTrips(items, past) {
 function loadTrips() {
     $.ajax('/api/past-trips?limit=12', {
         success: (response) => {
-            $("#past-trips").html(getHTMLForTrips(response.items, true))
+            $("#past-trips").html(getHTMLForTrips(response.trips, true))
         }
     })
     $.ajax('/api/upcoming-trips?limit=1', {
         success: (response) => {
-            $("#upcoming-trips").html(getHTMLForTrips(response.items, false))
+            $("#upcoming-trips").html(getHTMLForTrips(response.trips, false))
         }
     })
 }
@@ -88,7 +95,7 @@ function updateMapMarkers(boats) {
             boatListHTML += `<span class="title">${boat.vesselName}</span>`
             boatListHTML += `<span>Estado: ${boat.status == 0 ? "en movimiento" : "amarrado"}</span>`
             boatListHTML += `<span>Velocidad: ${boat.speed} knots</span>`
-            boatListHTML += `<span>Desde ${dayjs(boat.timestamp).format("MM/DD hh:mma")}`
+            boatListHTML += `<span>Desde ${dayjs(boat.timestamp).tz("America/Puerto_Rico").format("MM/DD hh:mma")}`
         boatListHTML += "</div>"
 
         // Get heading/course information
--- app/static.js
+++ app/static.js
@@ -1,4 +1,4 @@
-const MMSI_LIBRARY = {
+exports.MMSI_LIBRARY = {
     "breezy-point": "368037610",
     "mr-mason": "367481080",
     "mr-row": "367495520",
@@ -6,8 +6,7 @@ const MMSI_LIBRARY = {
     "summer-wind": "368038080",
     "isleno": "367617270"
 }
-
-const VESSEL_NAMES = {
+exports.VESSEL_NAMES = {
     "mr-mason": "Mr. Mason",
     "mr-row": "Mr. Row",
     "lady-eve": "Lady Eve",
@@ -15,8 +14,7 @@ const VESSEL_NAMES = {
     "summer-wind": "Summer Wind",
     "isleno": "Isleño"
 }
-
-const VESSEL_COLORS = {
+exports.VESSEL_COLORS = {
     "mr-mason": "black",
     "mr-row": "red",
     "lady-eve": "blue",
@@ -25,6 +23,26 @@ const VESSEL_COLORS = {
     "isleno": "green"
 }
 
-exports.MMSI_LIBRARY = MMSI_LIBRARY
-exports.VESSEL_NAMES = VESSEL_NAMES
-exports.VESSEL_COLORS = VESSEL_COLORS
+exports.CEIBA_PORT_GEOMETRY = { 
+    coordinates: [[ 
+        [ -65.625887, 18.234732 ], 
+        [ -65.612583, 18.226009 ], 
+        [ -65.618248, 18.218101 ], 
+        [ -65.631638, 18.227069 ], 
+        [ -65.625887, 18.234732 ] 
+    ]],
+    type: "Polygon"
+}
+exports.VIEQUES_PORT_GEOMETRY = {
+    coordinates: [[
+        [ -65.442767, 18.152968 ], 
+        [ -65.444248, 18.153518 ], 
+        [ -65.444956, 18.151867 ], 
+        [ -65.442982, 18.151296 ],
+        [ -65.442767, 18.152968 ]
+    ]],
+    type: "Polygon"
+}
+
+exports.AIS_MOORED = 5
+exports.AIS_UNDERWAY = 0
