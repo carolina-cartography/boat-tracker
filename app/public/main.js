8c06d677876d79fff3421f09ebd061143f4f87dd
Tomas Roy <hello@minicreative.net>
2021-12-18 23:22:51 -0400

    app: leaflet line fix

--- app/public/main.js
+++ app/public/main.js
@@ -72,6 +72,26 @@ function initializeMap() {
 
     // Zoom to position
     map.setView(CENTER, ZOOM)
+
+    // Do Leaflet line fix
+    leafletLineFix()
+}
+
+/* 
+* Workaround for 1px lines appearing in some browsers due to fractional transforms
+* and resulting anti-aliasing.
+* https://github.com/Leaflet/Leaflet/issues/3575
+*/
+function leafletLineFix(){
+    var originalInitTile = L.GridLayer.prototype._initTile
+    L.GridLayer.include({
+        _initTile: function (tile) {
+            originalInitTile.call(this, tile);
+            var tileSize = this.getTileSize();
+            tile.style.width = tileSize.x + 1 + 'px';
+            tile.style.height = tileSize.y + 1 + 'px';
+        }
+    });
 }
 
 async function loadTrips() {
